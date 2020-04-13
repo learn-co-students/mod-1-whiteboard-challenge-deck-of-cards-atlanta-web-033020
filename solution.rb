54ee4028ff442c4d22eafd7195dd01ddc7655934
rpdecks <rpdecks@gmail.com>
2020-04-13 09:58:44 -0400

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,29 @@
+
 class Deck
-
-end
-
-class Card
-
-end
+    attr_reader :cards
+  
+    def initialize
+      @cards = []
+      suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+      ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  
+      suits.each do |s|
+        ranks.each do |r|
+          cards << Card.new(s, r)
+        end
+      end
+    end
+  
+    def choose_card
+      cards.delete(cards.sample)
+    end
+  end
+  
+  class Card
+    attr_reader :suit, :rank
+  
+    def initialize(suit, rank)
+      @suit = suit
+      @rank = rank
+    end
+  end
